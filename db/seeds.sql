fe24ffba94a480c2f6417265c17cc9d7f317ee9b
KrystalCW <krystal.wiederrich@gmail.com>
2019-04-21 22:37:21 -0700

    added all initial files and basic coding

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ("Veggie Burger");
+INSERT INTO burgers (name) VALUES ("Cowboy burger");
+INSERT INTO burgers (name, eaten) VALUES ("Chilli burger", true);
+INSERT INTO burgers (name, eaten) VALUES ("Kiwi burger", true);
