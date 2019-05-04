7fb4a52d0fcebebf3ce2d4b1c95f3facdab9bcd0
KrystalCW <krystal.wiederrich@gmail.com>
2019-05-03 17:21:21 -0700

    removed comments

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -7,21 +7,18 @@ $(function() {
         eaten: newEaten
       };
   
-      // Send the PUT request.
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
         data: newEatenState
       }).then(
         function() {
           console.log("changed eaten to", newEaten);
-          // Reload the page to get the updated list
           location.reload();
         }
       );
     });
   
     $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
       var newBurger = {
@@ -29,14 +26,12 @@ $(function() {
         eaten: $("[name=eaten]:checked").val().trim()
       };
   
-      // Send the POST request.
       $.ajax("/api/burgers", {
         type: "POST",
         data: newBurger
       }).then(
         function() {
           console.log("created new burger");
-          // Reload the page to get the updated list
           location.reload();
         }
       );
